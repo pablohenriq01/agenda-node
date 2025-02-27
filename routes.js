2905fea5771a640e7108e7d82ce7a8191be9c955
PabloH <pablohenriqq01@gmail.com>
2025-02-27 17:45:32 -0300

    Iniciando tela de login e adicionando bootstrap nela

--- routes.js
+++ routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController.js');
-
+const loginController = require('./src/controllers/loginController.js')
+// Rotas da home
 route.get('/', homeController.index);
+
+// Rotas login
+route.get('/login/index', loginController.index);
 
 module.exports = route;
