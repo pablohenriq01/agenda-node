c641b66570d5d7c17932b3e5ead3b882015af85f
PabloH <pablohenriqq01@gmail.com>
2025-03-03 14:46:35 -0300

    Adicionando a tela, controller e rota de registro de usuarios

--- routes.js
+++ routes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController.js');
 const loginController = require('./src/controllers/loginController.js')
+const registerController = require('./src/controllers/registerController.js')
 // Rotas da home
 route.get('/', homeController.index);
 
 // Rotas login
 route.get('/login/index', loginController.index);
 
+//Rotas de cadastro
+route.get('/register/index', registerController.index)
 module.exports = route;
